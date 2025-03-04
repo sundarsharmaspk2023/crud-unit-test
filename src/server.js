f66418d1325ebc75e7407da3ea663479298402e9
sundarsharmaspk2023 <sundarsharmaspk2023@gmail.com>
2025-03-04 18:46:20 +0530

    sfddsf

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import connectDB from "./src/config/db.js";
-import userRoutes from "./src/routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
--- tests/user.test.js
+++ tests/user.test.js
@@ -7,9 +7,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 beforeAll(async () => {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+    await mongoose.connect("mongodb://localhost:27017/crudTest", {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
     });
 });
 
